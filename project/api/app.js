7e9172dcd212acf661f6aa409d2e5acf8eca3ff7
JacksonSearles <Jsearle1@binghamton.edu>
2025-09-10 01:35:37 -0400

    Project structure

--- project/api/app.js
+++ project/api/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 5001;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from backend!');
+  res.send('Hello from backend');
 });
 
 app.listen(port, () => {
--- project/app/pages/index.js
+++ project/app/pages/index.js
@@ -1,1 +1,2 @@
-export default function Home() { return <h1>Hello World</h1> }
+export default function Home() { return <h1>Hello from frontend</h1> }
+// Everythin in the pages folder is a frontend route
